32028c2fcfa36469ca98049b29c5a14138c6adb7
Hugo <hehlinge@e2r7p8.42.fr>
2019-04-12 16:07:31 +0200

    sa mere la pute, il faut des unsigned quand on fait mumuse avec les bits

--- srcs/ft_resolve.c
+++ srcs/ft_resolve.c
@@ -6,7 +6,7 @@
 /*   By: hehlinge <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/12 11:02:55 by hehlinge          #+#    #+#             */
-/*   Updated: 2019/04/12 13:35:58 by hehlinge         ###   ########.fr       */
+/*   Updated: 2019/04/12 16:06:38 by hehlinge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,31 @@ int		ft_check_shift(t_tetri *piece, int opt, int size)
 {
 	int		i;
 
+	//int debog = -1;
+	/*ft_putstr("piece avant\n");
+	while (++debog < 16)
+		ft_print_bits(piece->tetri[debog]);*/
 	if (opt == CHECK_RIGHT && piece->width + piece->x < size)
 	{
 		i = -1;
 		while (++i < 4)
-			piece->tetri[piece->y + i] >>= 1;
+		{
+			unsigned short tmp = (unsigned short)piece->tetri[piece->y + i];
+			ft_putstr("BEFORE\n");
+			tmp >>= 1;
+			ft_print_bits(tmp);
+			//piece->tetri[piece->y + i] = tmp;
+			/*
+			ft_putstr("AFTER \n");
+			ft_print_bits(piece->tetri[piece->y + i]);*/
+		}
 		(piece->x)++;
+
+	/*	debog = -1;
+		ft_putstr("piece avant\n");
+		while (++debog < 16)
+			ft_print_bits(piece->tetri[debog]);*/
+
 		return (EXIT_SUCCESS);
 	}
 	else if (opt == CHECK_DOWN && piece->width + piece->x < size)
@@ -39,17 +58,43 @@ int		ft_check_insert(t_tetri *map, t_tetri tetri)
 {
 	int		y;
 
-	y = 0;
-	while (y < 4)
+	y = 0;/*
+			 int debog = -1;
+			 ft_putstr("map \n");
+			 while (++debog < 16)
+			 ft_print_bits(map->tetri[debog]);
+			 debog = -1;
+			 ft_putstr("piece \n");
+			 while (++debog < 16)
+			 ft_print_bits(tetri.tetri[debog]);*/
+
+	while (y < tetri.height)
 	{
-		if (tetri.tetri[y + tetri.y] & map->tetri[y + tetri.y])
+		/*ft_putstr("map \n");
+		ft_print_bits(map->tetri[y + tetri.y]);
+		ft_putstr("piece \n");
+		ft_print_bits(tetri.tetri[y + tetri.y]);*/
+		if ((tetri.tetri[y + tetri.y] & map->tetri[y + tetri.y]))
+		{
+			ft_putstr("je me barre\n");
 			return (EXIT_FAILURE);
+		}
 		y++;
 	}
+
+	//int debog = -1;
+	//ft_putstr("map \n");
+	/*while (++debog < 16)
+	  ft_print_bits(map->tetri[debog]);
+	  debog = -1;*/
+	/*ft_putstr("piece \n");
+	  while (++debog < 16)
+	  ft_print_bits(tetri.tetri[debog]);*/
+
 	y = 0;
-	while (y < 4)
+	while (y < tetri.height)
 	{
-		tetri.tetri[y + tetri.y] ^= map->tetri[y + tetri.y];
+		map->tetri[y + tetri.y] ^= tetri.tetri[y + tetri.y];
 		y++;
 	}
 	return (EXIT_SUCCESS);
@@ -57,26 +102,43 @@ int		ft_check_insert(t_tetri *map, t_tetri tetri)
 
 int		ft_backtrack(t_tetri tab[NB_TETRI_MAX + 2], int nb_piece, int tetri, int size)
 {
-	int		i;
-
-	if (tetri == nb_piece)
+	//int		i;
+	//
+	/*ft_putchar('\n');
+	ft_putnbr(nb_piece);
+	ft_putnbr(tetri);
+	ft_putchar('\n');*/
+	if (tetri  == nb_piece)
 		return (EXIT_SUCCESS);
-	while (!ft_check_shift(&tab[tetri], CHECK_RIGHT, size))
+	if (ft_check_insert(&tab[nb_piece], tab[tetri]) == EXIT_SUCCESS)
 	{
-		while (!ft_check_shift(&tab[tetri], CHECK_DOWN, size))
-		{
-			if (ft_check_insert(&tab[nb_piece], tab[tetri]))
-				ft_backtrack(tab, nb_piece, ++tetri, size);
-		}
+		/*ft_putstr("piece inserree\n");
+		  int debog = -1;
+		  ft_putstr("map \n");
+		  while (++debog < 16)
+		  ft_print_bits(tab[nb_piece].tetri[debog]);*/
+
+		if (ft_backtrack(tab, nb_piece, ++tetri, size) == EXIT_SUCCESS)
+			return (EXIT_SUCCESS);
 	}
-	i = 0;
-	while (i < nb_piece)
+	else
 	{
-		ft_shift(tab[i].tetri, tab[i].x, tab[i].y);
-		tab[i].x = 0;
-		tab[i].y = 0;
-		i++;
+		while (ft_check_shift(&tab[tetri], CHECK_RIGHT, size) == EXIT_SUCCESS)
+			if (ft_backtrack(tab, nb_piece, tetri, size) == EXIT_SUCCESS)
+				return (EXIT_SUCCESS);
+		/*while (ft_check_shift(&tab[tetri], CHECK_DOWN, size) == EXIT_SUCCESS)
+		  if (ft_backtrack(tab, nb_piece, tetri, size) == EXIT_SUCCESS)
+		  return (EXIT_SUCCESS);*/
 	}
-	ft_backtrack(tab, nb_piece, 0, ++size);
+
+	/*i = 0;
+	  while (i < nb_piece)
+	  {
+	  ft_shift(tab[i].tetri, tab[i].x, tab[i].y);
+	  tab[i].x = 0;
+	  tab[i].y = 0;
+	  i++;
+	  }
+	  ft_backtrack(tab, nb_piece, 0, ++size);*/
 	return (EXIT_FAILURE);
 }
